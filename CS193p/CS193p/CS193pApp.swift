e979e9a211a3f8f7c0d7bc69ed9160e5efb7c2d9
benbenlang <benbenlang725@gmail.com>
2025-12-22 22:31:20 +0800

    cs193p

---
+++ CS193p/CS193p/CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193pApp.swift
+//  CS193p
+//
+//  Created by ydf on 2025/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193p/CS193pTests/CS193pTests.swift
@@ -0,0 +1,16 @@
+//
+//  CS193pTests.swift
+//  CS193pTests
+//
+//  Created by ydf on 2025/12/22.
+//
+
+import Testing
+
+struct CS193pTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
