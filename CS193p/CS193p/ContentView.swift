b79bd8a8e49f08287a3dcb7c6e08f2056acf4a93
benbenlang <benbenlang725@gmail.com>
2025-12-23 22:30:46 +0800

    !!

--- CS193p/CS193p/ContentView.swift
+++ CS193p/CS193p/ContentView.swift
@@ -11,9 +11,11 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
+                .foregroundColor(Color.orange)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world!!!")
+                .font(.largeTitle)
         }
         .padding()
     }
