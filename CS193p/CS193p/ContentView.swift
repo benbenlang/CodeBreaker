a77cf7bdf02ef16954c86f7f669b97c43805711e
benbenlang <benbenlang725@gmail.com>
2025-12-27 22:38:07 +0800

    Circle().foregroundStyle(colors[0])

--- CS193p/CS193p/ContentView.swift
+++ CS193p/CS193p/ContentView.swift
@@ -11,18 +11,18 @@ struct ContentView: View {
     var body: some View {
         VStack{
             pegs(colors: [.red, .green, .green, .yellow])
-            pegs(colors: [.red, .green, .green, .yellow])
-            pegs(colors: [.red, .green, .green, .yellow])
+            pegs(colors: [.red, .blue, .green, .red])
+            pegs(colors: [.red, .yellow, .green, .blue])
         }
         .padding()
     }
     
     func pegs(colors: Array<Color>) -> some View {
         HStack{
-            Circle().foregroundStyle(.red)
-            Circle().foregroundStyle(.green)
-            Circle().foregroundStyle(.green)
-            Circle().foregroundStyle(.yellow)
+            Circle().foregroundStyle(colors[0])
+            Circle().foregroundStyle(colors[1])
+            Circle().foregroundStyle(colors[2])
+            Circle().foregroundStyle(colors[3])
         }
     }
     
